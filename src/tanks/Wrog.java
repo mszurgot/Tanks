fefaa60d0697a6d6d0ce7556682bae973c0bc014
Wolverine111 <morfeusz454@gmail.com>
2015-01-18 13:04:09 +0100

    Player 2

--- src/tanks/Wrog.java
+++ src/tanks/Wrog.java
@@ -5,6 +5,9 @@
  */
 package tanks;
 
+import java.awt.event.KeyEvent;
+import static tanks.Pojazd.LEWO;
+
 /**
  *
  * @author Zet
@@ -14,10 +17,63 @@ public class Wrog extends Pojazd{
     public Wrog(int gridX, int gridY, int reload, int missileSpeed) {
         super(gridX, gridY, reload, missileSpeed);
     }
+    
+    @Override
+    public void setTankNumber(int tankNumber) {
+        super.setTankNumber(tankNumber); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void keyPressed(KeyEvent e) {
 
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_Q) {
+            fire(this);
+        }
+
+        if ((key == KeyEvent.VK_A) && !ruchWGore && !ruchWDol) {
+            if (kierunek == LEWO) {
+                if (!ruchWLewo) {
+                    ruchWLewo = true;
+                }
+            } else {
+                kierunek = LEWO;
+                displayedImage = imageTab[3];
+            }
+        } else if ((key == KeyEvent.VK_D) && !ruchWGore && !ruchWDol) {
+            if (kierunek == PRAWO) {
+                if (!ruchWPrawo) {
+                    ruchWPrawo = true;
+                }
+            } else {
+                kierunek = PRAWO;
+                displayedImage = imageTab[2];
+            }
+        } else if ((key == KeyEvent.VK_W) && !ruchWLewo && !ruchWPrawo) {
+            if (kierunek == GORA) {
+                if (!ruchWGore) {
+                    ruchWGore = true;
+                }
+            } else {
+                kierunek = GORA;
+                displayedImage = imageTab[0];
+            }
+        } else if ((key == KeyEvent.VK_S) && !ruchWLewo && !ruchWPrawo) {
+            if (kierunek == DOL) {
+                if (!ruchWDol) {
+                    ruchWDol = true;
+                }
+            } else {
+                kierunek = DOL;
+                displayedImage = imageTab[1];
+            }
+        }
+    }
+
+    
     @Override
     public void makeMove() {   
-        initRandomAIMoves();
+       // initRandomAIMoves();
         move();
     }
     private void initRandomAIMoves(){
@@ -28,7 +84,9 @@ public class Wrog extends Pojazd{
         W tej klasie wszystkie te akcje będą musiały być generowane przez AI w tym miejscu przed wykonaniem move.
         Ta metoda jest wywolywana co takt.
         */
-    
+        
     }
+    
+    
     
 }
